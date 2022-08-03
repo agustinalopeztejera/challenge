3ded25d810a57c40951fea459753613c18de9543
Agustina Lopez Tejera <agustinalopeztejera@gmail.com>
2022-08-03 10:45:59 -0300

    desafio_terminado

--- family/views.py
+++ family/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from datetime import datetime
 
 from family.models import Family_members
 
 def family_member(request):
-    family_member = Family_members.objects.create(name = 'John Smith', age = 50, hobbies = 'jugar al futbol', birthday = 1972)
+    family_member = Family_members.objects.create(name = 'John Smith', age = 50, hobbies = 'jugar al futbol', birthday = datetime(1972,2,19))
     context = {
         'family_member':family_member
     }
